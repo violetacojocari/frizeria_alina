c7a2b818f8dccbfaa27450c35e561a012ed643f4
violetacojocari <128294292+violetacojocari@users.noreply.github.com>
2023-04-06 13:47:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const header = document.querySelector(".navbar");
+if (window.innerWidth < 768) {
+  header.classList.add("sticky");
+}
+
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 0);
+  if (window.innerWidth >= 768) {
+    header.classList.toggle("sticky", window.scrollY > 0);
+  }
 });
 
 function show() {
   document.getElementById("side-menu").classList.toggle("shows");
+}
+function hide() {
+  document.getElementById("side-menu").classList.remove("shows");
 }
